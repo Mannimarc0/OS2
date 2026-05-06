41a1e1814d8bf7852e5f6fcde4c0622ff1996b80
Maxim <makskross184@gmail.com>
2026-05-06 18:42:32 +0300

    Update OS2.2.cpp

--- OS2.2.cpp
+++ OS2.2.cpp
@@ -1,16 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <signal.h>
-#include <aio.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <stdint.h>
-#include <inttypes.h>
-#include <time.h>
+#include <stdio.h>      
+#include <stdlib.h>     
+#include <string.h>     
+#include <fcntl.h>      
+#include <unistd.h>     
+#include <sys/mman.h>   
+#include <sys/stat.h>   
+#include <sys/time.h>   
+#include <sys/select.h> 
 int fd;
 void *ptr;
 #define FILENAME "shared_mem.bin"
